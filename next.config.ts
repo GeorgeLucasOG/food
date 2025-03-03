24fce747440a263dd3cea498c7918acef8a936ba
lucas.goncalves <lucas.goncalves@govone.digital>
2025-03-03 16:09:21 -0300

    feat: adicionar novos padrões de imagens remotas para Next.js

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }],
+    remotePatterns: [
+      { hostname: "u9a6wmr3as.ufs.sh" },
+      { hostname: "img.cardapio.menu" },
+      { hostname: "images2.imgbox.com" },
+      { hostname: "img.freepik.com" },
+    ],
   },
 };
 
