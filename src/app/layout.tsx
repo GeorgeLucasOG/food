526c542a141e4bdd9fab38e7dd371aa2b164ade1
lucas.goncalves <lucas.goncalves@govone.digital>
2025-03-03 21:06:20 -0300

    refactor: atualizar metadados do layout com novo título e descrição

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "FSW Donalds",
-  description: "Bora finalizar esse projeto lindo!",
+  title: "George Lucas DEV",
+  description: "Menu Interativo",
 };
 
 export default function RootLayout({
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body className={`${poppins.className} antialiased`}>
         <CartProvider>{children}</CartProvider>
 
-        <Toaster/>
+        <Toaster />
       </body>
     </html>
   );
